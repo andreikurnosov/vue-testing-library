2ca773615db9937e88e2a315d4802e657e25b34c
andreikurnosov <nixdie@yandex.ru>
2021-04-27 11:43:00 +0700

    init commit & first test

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,12 +1,15 @@
-import { shallowMount } from '@vue/test-utils'
+import { render } from '@testing-library/vue'
+import '@testing-library/jest-dom'
 import HelloWorld from '@/components/HelloWorld.vue'
 
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
+
+    const { getByText } = render(HelloWorld, {
       props: { msg }
     })
-    expect(wrapper.text()).toMatch(msg)
+
+    expect(getByText(msg)).toBeInTheDocument()
   })
 })
